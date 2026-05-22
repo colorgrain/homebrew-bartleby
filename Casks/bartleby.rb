75c7788a549f6a43268ffe86af90c50b7873a68b
colorgrain <a.westphal@ik.me>
2026-05-22 10:53:56 +0200

    chore: update sha256 for rebuilt v0.1.0-5 dmg

--- Casks/bartleby.rb
+++ Casks/bartleby.rb
@@ -1,6 +1,6 @@
 cask "bartleby" do
   version "0.1.0-5"
-  sha256 "368e3c26551c2250b269ba31ac028e629a05f9fe5cd746607fcb227491de9034"
+  sha256 "8d7164ef71f0a7e534423966a9546b423f544da7b3217970fb5674b9d13a2fc1"
 
   url "https://github.com/colorgrain/Bartleby/releases/download/v#{version}/Bartleby_#{version}_aarch64.dmg"
   name "Bartleby"
